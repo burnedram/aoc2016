57536216b1d4de61d1fd46114b9fe7729fb49c02
Rafael Mohlin <raffe_mohlin@hotmail.com>
2016-12-06 07:34:47 +0100

    Made day06 parallel. Very important, one iteration takes like 2ms to compute on a single thread

--- aoc2016/src/aoc2016/days/Day06.cs
+++ aoc2016/src/aoc2016/days/Day06.cs
@@ -10,17 +10,40 @@ namespace aoc2016.day06
         public static void Run()
         {
             string[] input = System.IO.File.ReadAllLines("day06.txt");
-            var errchecked = input.Aggregate(Enumerable.Range(0, input[0].Length).Select(i => new Dictionary<char, int>()).ToList(), (err, nx) =>
-            {
-                for (int i = 0; i < nx.Length; i++)
+            int nxlen = input[0].Length;
+            var errchecked = Enumerable.Range(0, nxlen).Select(i => new Dictionary<char, int>()).ToList();
+            Parallel.ForEach(input, () => Enumerable.Range(0, nxlen).Select(i => new Dictionary<char, int>()).ToList(),
+                (nx, loopState, partialResult) =>
                 {
-                    if (err[i].ContainsKey(nx[i]))
-                        err[i][nx[i]] = err[i][nx[i]] + 1;
-                    else
-                        err[i][nx[i]] = 1;
+                    for (int i = 0; i < nxlen; i++)
+                    {
+                        var ec = partialResult[i];
+                        if (ec.ContainsKey(nx[i]))
+                            ec[nx[i]] = ec[nx[i]] + 1;
+                        else
+                            ec[nx[i]] = 1;
+                    }
+                    return partialResult;
+                },
+                (localPartial) =>
+                {
+                    lock(errchecked)
+                    {
+                        for (int i = 0; i < nxlen; i++)
+                        {
+                            var ec = errchecked[i];
+                            var lp = localPartial[i];
+                            foreach (var kv in lp)
+                            {
+                                if (ec.ContainsKey(kv.Key))
+                                    ec[kv.Key] = ec[kv.Key] + kv.Value;
+                                else
+                                    ec[kv.Key] = kv.Value;
+                                }
+                        }
+                    }
                 }
-                return err;
-            });
+            );
 
             string part1 = new string(errchecked.Select(dict => dict.OrderByDescending(kv => kv.Value).First().Key).ToArray());
             Console.WriteLine("==== Part 1 ====");
